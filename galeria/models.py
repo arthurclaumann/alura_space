10f92c03b500117349c8acd39e114761f095f703
Arthur Schmidt <arthur.claumann@gmail.com>
2023-11-07 17:41:34 -0300

    criação do banco de dados e tornando os dados dinamicos

--- galeria/models.py
+++ galeria/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+# Here we create our models to be used in html
+# The class represents a table in database, after this, we will migrate our class to a database, with commando line python manage.py makemigrations and then command migrate
+
+class Fotografia(models.Model):
+    nome = models.CharField(max_length= 100, null = False, blank = False)
+    legenda = models.CharField(max_length= 150, null = False, blank = False)
+    descricao = models.TextField(null=False, blank = False)
+    foto = models.CharField(max_length= 100, null = False, blank = False)
+
+    # Creating a good practice
+    def __str__(self):
+        return f'Fotografia [nome={self.nome}]'
--- galeria/views.py
+++ galeria/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
+from galeria.models import Fotografia
 
 def index(request):
-    return render(request, 'galeria/index.html')
+
+    # Iremos enviar o dicionário dados para o index html através da função render, mas para passarmos os dados para o software, ao invés de criarmos uma variável com os dados, iremos acessar os dados da base de dados
+
+    # dados = {
+    # 1: {"nome":"Nebulosa de Carina",
+    #     'legenda': 'webbtelescope.org / NASA / James Webb'},
+    # 2: {'nome': 'Galaxia NCG 1079',
+    #     'legenda': 'nasa.org / NASA / Hubble'}
+    # }
+    fotografias = Fotografia.objects.all()
+    return render(request, 'galeria/index.html', {'cards': fotografias})
 
 
 def imagem(request):
